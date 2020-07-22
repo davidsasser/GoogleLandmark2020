65068d5dff421d13595ca8e6d18dc184bf4fbb9b
David Sasser <david.alan.sasser@gmail.com>
2020-07-21 19:47:53 -0500

    initial commit

---
+++ train_model.py
@@ -0,0 +1,55 @@
+import tensorflow as tf
+# from keras.layers import Input, Conv2D, MaxPooling2D, UpSampling2D, ZeroPadding2D
+# from keras.models import Model
+# from keras.callbacks import TensorBoard
+# from keras.datasets import mnist
+import numpy as np
+
+(x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
+
+x_train = x_train.astype('float32') / 255.
+x_test = x_test.astype('float32') / 255.
+x_train = np.reshape(x_train, (len(x_train), 28, 28, 1))  # adapt this if using `channels_first` image data format
+x_test = np.reshape(x_test, (len(x_test), 28, 28, 1))  # adapt this if using `channels_first` image data format
+
+# np.save('x_train')
+
+noise_factor = 0.5
+x_train_noisy = x_train + noise_factor * np.random.normal(loc=0.0, scale=1.0, size=x_train.shape)
+x_test_noisy = x_test + noise_factor * np.random.normal(loc=0.0, scale=1.0, size=x_test.shape)
+
+x_train_noisy = np.clip(x_train_noisy, 0., 1.)
+x_test_noisy = np.clip(x_test_noisy, 0., 1.)
+
+
+def train_model():
+    input_img = tf.keras.layers.Input(shape=(28, 28, 1))  # adapt this if using `channels_first` image data format
+    x = tf.keras.layers.Conv2D(16, (3, 3), activation='relu', padding='same')(input_img)
+    x = tf.keras.layers.MaxPooling2D((2, 2), padding='same')(x)
+    x = tf.keras.layers.Conv2D(8, (3, 3), activation='relu', padding='same')(x)
+    x = tf.keras.layers.MaxPooling2D((2, 2), padding='same')(x)
+    x = tf.keras.layers.Conv2D(8, (3, 3), activation='relu', padding='same')(x)
+    encoded = tf.keras.layers.MaxPooling2D((2, 2), padding='same', name='encoder')(x)
+
+    # at this point the representation is (4, 4, 8) i.e. 128-dimensional
+
+    x = tf.keras.layers.Conv2D(8, (3, 3), activation='relu', padding='same')(encoded)
+    x = tf.keras.layers.UpSampling2D((2, 2))(x)
+    x = tf.keras.layers.Conv2D(8, (3, 3), activation='relu', padding='same')(x)
+    x = tf.keras.layers.UpSampling2D((2, 2))(x)
+    x = tf.keras.layers.Conv2D(16, (3, 3), activation='relu')(x)
+    x = tf.keras.layers.UpSampling2D((2, 2))(x)
+    decoded = tf.keras.layers.Conv2D(1, (3, 3), activation='sigmoid', padding='same')(x)
+
+    autoencoder = tf.keras.models.Model(input_img, decoded)
+    autoencoder.compile(optimizer='adadelta', loss='binary_crossentropy')
+
+    autoencoder.fit(x_train_noisy, x_train,
+                    epochs=20,
+                    batch_size=128,
+                    shuffle=True,
+                    validation_data=(x_test_noisy, x_test))
+
+    autoencoder.save('autoencoder.h5')
+
+train_model()
